ebd3bf5b6d44a3cf911ab9f9c497f4ca9a32c431
bsvega <bsvega@bancolombia.com.co>
2024-06-11 23:14:17 -0500

    add findByName on ProductRepository

--- domain/model/src/main/java/co/com/the_chaos_company/model/product/gateways/ProductRepository.java
+++ domain/model/src/main/java/co/com/the_chaos_company/model/product/gateways/ProductRepository.java
@@ -1,4 +1,7 @@
 package co.com.the_chaos_company.model.product.gateways;
 
+import co.com.the_chaos_company.model.product.Product;
+
 public interface ProductRepository {
+    Product findByName(String name);
 }
--- domain/usecase/src/main/java/co/com/the_chaos_company/usecase/product/ProductUseCase.java
+++ domain/usecase/src/main/java/co/com/the_chaos_company/usecase/product/ProductUseCase.java
@@ -1,6 +1,14 @@
 package co.com.the_chaos_company.usecase.product;
 
+import co.com.the_chaos_company.model.product.Product;
+import co.com.the_chaos_company.model.product.gateways.ProductRepository;
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 public class ProductUseCase {
+    private final ProductRepository productRepository;
+
+    public Product findByName(String name) {
+        return productRepository.findByName(name);
+    }
 }
