0edb40005558ebb7c7931691d5cb2a8fa6cfd9b5
Brayan Sebastian Vega Garcia <bsvega@bancolombia.com.co>
2025-02-19 20:46:52 -0500

    cleanup code

--- domain/usecase/src/main/java/co/com/the_chaos_company/usecase/cartitem/CartItemUseCase.java
+++ domain/usecase/src/main/java/co/com/the_chaos_company/usecase/cartitem/CartItemUseCase.java
@@ -1,6 +1,7 @@
 package co.com.the_chaos_company.usecase.cartitem;
 
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 public class CartItemUseCase {
 }
--- domain/usecase/src/main/java/co/com/the_chaos_company/usecase/shoppingcart/ShoppingCartUseCase.java
+++ domain/usecase/src/main/java/co/com/the_chaos_company/usecase/shoppingcart/ShoppingCartUseCase.java
@@ -1,6 +1,7 @@
 package co.com.the_chaos_company.usecase.shoppingcart;
 
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 public class ShoppingCartUseCase {
 }
